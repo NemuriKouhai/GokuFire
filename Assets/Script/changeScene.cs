3f1f674f16ba847e3428377dbc385cc48ccb32c7
yomogimochi139 <ryouhei6713@icloud.com>
2025-06-28 14:38:19 +0900

    タイトルからステージに移動追加

---
+++ Assets/Script/changeScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public void change_button() //change_button‚Æ‚¢‚¤–¼‘O‚É‚µ‚Ü‚·
+    {
+        SceneManager.LoadScene("StageTsukuri");//second‚ğŒÄ‚Ño‚µ‚Ü‚·
+    }
+}
