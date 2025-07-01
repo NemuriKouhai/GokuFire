a044df8adea11f0a729af1258715d8ef96fa2ea9
raytrek2-\ktc <>
2025-07-01 14:40:22 +0900

    ゲームクリア・ゲームオーバーからタイトルに移動追加

--- Assets/change.cs
+++ Assets/change.cs
@@ -4,8 +4,12 @@ using UnityEngine.SceneManagement;
 public class change : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void change_button() 
+     public void change_button()
     {
         SceneManager.LoadScene("StageTsukuri");
+    }
+    public void button()
+    {
+        SceneManager.LoadScene("Title");
     }
 }
